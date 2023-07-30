698cbdff334070e35b885a643e27f5cf6d31c638
BruceNirun19 <nirun.h@planetcomm.com>
2023-07-30 16:17:25 +0700

    const

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import "fmt"
 
+// นิยายตัวแปร
+// func main() {
+// 	name := "brucenirun"
+// 	var age int = 25
+// 	var score float32 = 65.5
+// 	isPass := true
+// 	fmt.Println("My name is", name)
+// 	fmt.Println("My age =", age)
+// 	fmt.Println("My score =", score)
+// 	fmt.Println("Pass Exam", isPass)
+// }
+
+//นิยามค่าคงที่
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println("BruceNirun")
+	const name string = "BruceNirun"
+	fmt.Println("My Name is", name)
 }
-  
